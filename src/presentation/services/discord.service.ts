96104b6fdd3b8458640e6fca078605218ea58ee7
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-09-24 02:57:36 -0500

    339. Enviar gif animado a Discord

--- src/presentation/services/discord.service.ts
+++ src/presentation/services/discord.service.ts
@@ -8,6 +8,13 @@ export class DiscordService {
 	async notify(message: string) {
 		const body = {
 			content: message
+			// embeds: [
+			// 	{
+			// 		image: {
+			// 			url: 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbHB4YXNma250MWxtcGN0anIzMTRrZ2NjdjI1cGJiZ2phc2x4aG5mYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qp61kl8rdZwuQ/giphy.gif'
+			// 		}
+			// 	}
+			// ]
 		};
 
 		const resp = await fetch(this.discordWebhookUrl, {
